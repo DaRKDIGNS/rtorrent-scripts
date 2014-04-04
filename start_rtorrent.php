03846db5f7a83f61232ec3742faa09bdec456303
jonnyboy <jonnyboy@jbmatrix.net>
2014-04-04 16:13:15 -0400

    updated start_rtorrent.php

--- start_rtorrent.php
+++ start_rtorrent.php
@@ -13,6 +13,14 @@
 // If you have a functioning email, email will be sent to notify
 // restarting of any rtorrent session only
 
+if (!isset($argv[1]) || ($argv[1] !== "exit" && $argv[1] !== "kill" && $argv[1] !== "start" && $argv[1] !== "cron")) {
+	exit("\nThis script will start multiple rtorrent sessions.\n\n"
+		. "php $argv[0] start    ...: To start all rtorrent sessions.\n"
+		. "php $argv[0] cron     ...: To start all rtorrent sessions but not attach to the tmux session.\n"
+		. "php $argv[0] kill     ...: To close all rtorrent sessions but leave tmux session running.\n"
+		. "php $argv[0] exit     ...: To close all rtorrent sessions and close the tmux session.\n");
+}
+
 // Start tmux server
 exec("tmux start-server");
 
@@ -22,7 +30,9 @@ $tmux_session = "rtorrent";
 //check if session exists
 $exec = exec("tmux list-session | grep $tmux_session", $session);
 if (count($session) !== 0) {
-	if (isset($argv[1]) && ($argv[1] === "kill" || $argv[1] === "exit")) {
+	// If there is a tmux session already
+	if ($argv[1] === "kill" || $argv[1] === "exit") {
+		// Tell rtorrent to close the session
 		exec("tmux send-keys -t $tmux_session:0 C-q");
 		exec("tmux send-keys -t $tmux_session:1 C-q");
 		exec("tmux send-keys -t $tmux_session:2 C-q");
@@ -42,13 +52,15 @@ if (count($session) !== 0) {
 					}
 				}
 			}
-			sleep(10);
-			echo ".";
+			if ($dead === false) {
+				sleep(10);
+				echo ".";
+			}
 		}
 		echo "\nAll rtorrent sessions terminated.\n";
-		if (isset($argv[1]) && $argv[1] === "exit") {
-			echo "tmux session $tmux_session has been terminated.\n";
+		if ($argv[1] === "exit") {
 			exec("tmux kill-session -t $tmux_session");
+			exit("\ntmux session $tmux_session has been terminated.\n\n");
 		}
 		exit();
 	} else {
@@ -74,36 +86,32 @@ if (count($session) !== 0) {
 			exec("tmux respawnp -t $tmux_session:4 'rtorrent -n -o import=~/.rtorrent-4.rc'");
 		}
 		if (shell_exec("tmux list-panes -t $tmux_session:8 | grep -c dead") == 1) {
-			if ((isset($argv[1]) && $argv[1] !== 'cron') || !isset($argv[1])) {
+			if ((isset($argv[1]) && $argv[1] !== "cron") || !isset($argv[1])) {
 				exec("tmux respawnp -t $tmux_session:8 'chromium-browser'");
 			}
 		}
-		if ((isset($argv[1]) && $argv[1] !== 'cron') || !isset($argv[1])) {
+		if ($argv[1] === "start") {
 			exec("tmux a -t $tmux_session");
 		}
 	}
 } else {
-	if (isset($argv[1]) && $argv[1] === "kill") {
-		echo "There is no session to kill.\n";
-		sleep(2);
-		exit();
-	} elseif (isset($argv[1]) && $argv[1] === "exit") {
-		echo "There is no session to exit.\n";
-		sleep(2);
-		exit();
-	}
-
-	exec("tmux -f ~/.tmux.conf new-session -d -s $tmux_session -n 0 'rtorrent -n -o import=~/.rtorrent.rc'");
-	exec("tmux new-window -t $tmux_session:1 -n 1 'rtorrent -n -o import=~/.rtorrent-1.rc'");
-	exec("tmux new-window -t $tmux_session:2 -n 2 'rtorrent -n -o import=~/.rtorrent-2.rc'");
-	exec("tmux new-window -t $tmux_session:3 -n 3 'rtorrent -n -o import=~/.rtorrent-3.rc'");
-	exec("tmux new-window -t $tmux_session:4 -n 4 'rtorrent -n -o import=~/.rtorrent-4.rc'");
-	exec("tmux new-window -t $tmux_session:5 -n htop 'htop'");
-	exec("tmux new-window -t $tmux_session:6 -n vnstat 'watch -n30 \"vnstat -u && vnstat -i eth0\"'");
-	exec("tmux selectp -t 0; tmux splitw -t $tmux_session:6 -h -p 50 'vnstat -l'");
-	exec("tmux new-window -t $tmux_session:7 -n bash 'bash -i'");
-	exec("tmux new-window -t $tmux_session:8 -n chromium 'chromium-browser'");
-	if ((isset($argv[1]) && $argv[1] !== 'cron') || !isset($argv[1])) {
-		exec("tmux select-window -t $tmux_session:7; tmux attach-session -d -t $tmux_session");
+	if ($argv[1] === "kill") {
+		exit("\nThere is no tmux session running and therefore there are no rtorrent sessions to terminate.\n\n");
+	} elseif ($argv[1] === "exit") {
+		exit("\nThere is no tmux session to terminate.\n\n");
+	} else {
+		exec("tmux -f ~/.tmux.conf new-session -d -s $tmux_session -n 0 'rtorrent -n -o import=~/.rtorrent.rc'");
+		exec("tmux new-window -t $tmux_session:1 -n 1 'rtorrent -n -o import=~/.rtorrent-1.rc'");
+		exec("tmux new-window -t $tmux_session:2 -n 2 'rtorrent -n -o import=~/.rtorrent-2.rc'");
+		exec("tmux new-window -t $tmux_session:3 -n 3 'rtorrent -n -o import=~/.rtorrent-3.rc'");
+		exec("tmux new-window -t $tmux_session:4 -n 4 'rtorrent -n -o import=~/.rtorrent-4.rc'");
+		exec("tmux new-window -t $tmux_session:5 -n htop 'htop'");
+		exec("tmux new-window -t $tmux_session:6 -n vnstat 'watch -n30 \"vnstat -u && vnstat -i eth0\"'");
+		exec("tmux selectp -t 0; tmux splitw -t $tmux_session:6 -h -p 50 'vnstat -l'");
+		exec("tmux new-window -t $tmux_session:7 -n bash 'bash -i'");
+		exec("tmux new-window -t $tmux_session:8 -n chromium 'chromium-browser'");
+		if ($argv[1] === "start") {
+			exec("tmux select-window -t $tmux_session:7; tmux attach-session -d -t $tmux_session");
+		}
 	}
 }
